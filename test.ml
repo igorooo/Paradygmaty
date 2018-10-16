02fa7b3ad85f4dd6112eb96ac01bb3a7a79111fd
QiviPC <igorczeczot@windowslive.com>
2018-10-16 16:11:02 +0200

    pc

--- test.ml
+++ test.ml
@@ -2,3 +2,8 @@ let rec count(a, xs) = if xs=[] then 0
 else if List.hd xs = a then 1+(count(a, List.tl xs))
 else count(a, List.tl xs);;
 count ('a', ['a'; 'l'; 'a']);;
+
+
+let rec power(n) = if n = 0 then 1 else n*power(n-1);;
+
+power(3)
